f407443a1f1b4ca56ca2e260d0745151fb86b922
Larry Hsiao <larryhsiao@silverhetch.com>
2019-02-08 00:26:06 +0800

    dependency robolectric test

---
+++ app/src/test/java/com/silverhetch/dashboard128/RobolectricTest.kt
@@ -0,0 +1,17 @@
+package com.silverhetch.dashboard128
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+/**
+ * Simple Robolectric test for framework availability.
+ */
+@RunWith(RobolectricTestRunner::class)
+class RobolectricTest {
+  @Test
+  fun simple() {
+    Assert.assertTrue(true)
+  }
+}
